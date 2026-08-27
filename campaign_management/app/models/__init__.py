bf3cc5a9837518a7e382226b4c2d92cc9ccfc635
Yuk1n3IzHav <tmzerogaming@gmail.com>
2026-08-27 07:50:34 +0700

    Delete campaign_management directory

--- campaign_management/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.db.database import Base, engine
-
-from .campaign import Campaign
-from .campaign_member import CampaignMember
-from .campaign_task import CampaignTask
-from .user import User
-from .campaign_audit_log import CampaignAuditLog
